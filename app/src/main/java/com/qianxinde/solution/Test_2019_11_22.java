ebc0e44c4c9f164600eda85ac56020adb3a4923d
yangbangwei <754667850@qq.com>
2019-11-22 14:25:17 +0800

    100. 相同的树 核心点： 第一，递归调用，先父节点，然后左右节点判断

--- app/src/main/java/com/qianxinde/solution/Test_2019_11_22.java
+++ app/src/main/java/com/qianxinde/solution/Test_2019_11_22.java
@@ -85,4 +85,36 @@ public class Test_2019_11_22 {
         }
         System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
     }
+
+    /**
+     * 100. 相同的树
+     * 给定两个二叉树，编写一个函数来检验它们是否相同。
+     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     *
+     * @param p 二叉树1
+     * @param q 二叉树2
+     * @return 返回是否相同
+     */
+    private static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (q == null && p == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 }
