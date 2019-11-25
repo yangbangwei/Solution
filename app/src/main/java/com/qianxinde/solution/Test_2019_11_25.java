bc11a7a94717653349a490e1e484488933d2eecc
yangbangwei <754667850@qq.com>
2019-11-25 11:31:45 +0800

    104. 二叉树的最大深度 核心点： 第一，递归调用，判断是否为末尾节点

--- app/src/main/java/com/qianxinde/solution/Test_2019_11_25.java
+++ app/src/main/java/com/qianxinde/solution/Test_2019_11_25.java
@@ -45,4 +45,53 @@ public class Test_2019_11_25 {
     private static boolean canWinNim(int n) {
         return n % 4 != 0;
     }
+
+    /**
+     * 104. 二叉树的最大深度
+     * 给定一个二叉树，找出其最大深度。
+     * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+     * 说明: 叶子节点是指没有子节点的节点。
+     *
+     * @param root 当前树
+     * @return 返回最大深度
+     */
+    private static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return countDepth(1, root);
+    }
+
+    /**
+     * 统计树的深度
+     *
+     * @param length 上一级深度
+     * @param root   当前节点
+     * @return 返回当前深度
+     */
+    private static int countDepth(int length, TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return length;
+        }
+        length++;
+        if (root.left == null) {
+            return countDepth(length, root.right);
+        }
+        if (root.right == null) {
+            return countDepth(length, root.left);
+        }
+        int num1 = countDepth(length, root.left);
+        int num2 = countDepth(length, root.right);
+        return num1 >= num2 ? num1 : num2;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 }
