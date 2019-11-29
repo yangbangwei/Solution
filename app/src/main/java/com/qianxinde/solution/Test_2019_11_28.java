dabf7b9f98970bb509a572c046d2073f9a423c3f
yangbangwei <754667850@qq.com>
2019-11-29 09:16:19 +0800

    263. 丑数 核心点： 第一，取余，除以2，3，5。

--- app/src/main/java/com/qianxinde/solution/Test_2019_11_28.java
+++ app/src/main/java/com/qianxinde/solution/Test_2019_11_28.java
@@ -90,4 +90,30 @@ public class Test_2019_11_28 {
             nums[i] = 0;
         }
     }
+
+    /**
+     * 263. 丑数
+     * 编写一个程序判断给定的数是否为丑数。
+     * 丑数就是只包含质因数 2, 3, 5 的正整数。
+     *
+     * @param num 任意数
+     * @return 是否为丑数
+     */
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 5 == 0) {
+                num /= 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
