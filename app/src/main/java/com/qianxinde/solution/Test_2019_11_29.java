6ff6475665fb0ad310f0a2fdfea41275fa7ad601
yangbangwei <754667850@qq.com>
2019-11-29 14:13:25 +0800

    374. 猜数字大小 核心点： 第一，二分法，防止Int类型溢出。 第二，三分法，mid和mid1。

--- app/src/main/java/com/qianxinde/solution/Test_2019_11_29.java
+++ app/src/main/java/com/qianxinde/solution/Test_2019_11_29.java
@@ -131,4 +131,78 @@ public class Test_2019_11_29 {
             val = x;
         }
     }
+
+    /**
+     * 374. 猜数字大小
+     * 我们正在玩一个猜数字游戏。 游戏规则如下：
+     * 我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+     * 每次你猜错了，我会告诉你这个数字是大了还是小了。
+     * 你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+     * 方法1，二分法，low+（high-low)/2防止Int类型溢出
+     *
+     * @param n 最大范围
+     * @return 猜对的值
+     */
+    private static int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int res = guess(mid);
+            if (res == 0) {
+                return mid;
+            } else if (res < 0) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 374. 猜数字大小
+     * 我们正在玩一个猜数字游戏。 游戏规则如下：
+     * 我从 1 到 n 选择一个数字。 你需要猜我选择了哪个数字。
+     * 每次你猜错了，我会告诉你这个数字是大了还是小了。
+     * 你调用一个预先定义好的接口 guess(int num)，它会返回 3 个可能的结果（-1，1 或 0）：
+     * 方法2，三分法
+     *
+     * @param n 最大范围
+     * @return 猜对的值
+     */
+    private static int guessNumber1(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 3;
+            int mid1 = high - (high - low) / 3;
+            int res = guess(mid);
+            int res1 = guess(mid1);
+            if (res == 0) {
+                return mid;
+            } else if (res1 == 0) {
+                return mid1;
+            } else if (res < 0) {
+                high = mid - 1;
+            } else if (res1 > 0) {
+                low = mid1 + 1;
+            } else {
+                low = mid + 1;
+                high = mid1 - 1;
+            }
+        }
+        return -1;
+    }
+
+    private static int guess(int num) {
+        int result = 10;
+        if (num > result) {
+            return 1;
+        } else if (num < result) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
