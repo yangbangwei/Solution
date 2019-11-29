d0303f5454ba35f5ebb335aa3f3db676a7edbc83
yangbangwei <754667850@qq.com>
2019-11-29 10:38:51 +0800

    242. 有效的字母异位词 核心点： 第一，排序后对比。 第二，根据出现字母出现次数比较。

--- app/src/main/java/com/qianxinde/solution/Test_2019_11_29.java
+++ app/src/main/java/com/qianxinde/solution/Test_2019_11_29.java
@@ -1,5 +1,7 @@
 package com.qianxinde.solution;
 
+import java.util.Arrays;
+
 /**
  * @author :yangbw
  * @date :2019-11-26
@@ -7,7 +9,8 @@ package com.qianxinde.solution;
 public class Test_2019_11_29 {
 
     public static void main(String[] args) {
-
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram1("aaaa", "a"));
     }
 
     private static class ListNode {
@@ -46,5 +49,52 @@ public class Test_2019_11_29 {
             i++;
             j--;
         }
+    }
+
+    /**
+     * 242. 有效的字母异位词
+     * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+     * 方法1，排序后对比
+     *
+     * @param s 字符串s
+     * @param t 字符串t
+     * @return 是否为异位词
+     */
+    private static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] char1 = s.toCharArray();
+        char[] char2 = t.toCharArray();
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+
+        return new String(char1).equals(new String(char2));
+    }
+
+    /**
+     * 242. 有效的字母异位词
+     * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+     * 方法2，通过出现次数判断
+     *
+     * @param s 字符串s
+     * @param t 字符串t
+     * @return 是否为异位词
+     */
+    private static boolean isAnagram1(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
