e0f77ad9f08aa793a40b55d31130746542fe7c47
yangbangwei <754667850@qq.com>
2019-12-03 10:44:00 +0800

    589. N叉树的前序遍历 核心点： 第一，递归调用。 第二，迭代法。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_03.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_03.java
@@ -1,5 +1,9 @@
 package com.qianxinde.solution.date_2019_12;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @author :yangbw
  * @date :2019-12-03
@@ -38,5 +42,67 @@ public class Test_2019_12_03 {
             }
         }
         return total;
+    }
+
+    /**
+     * 589. N叉树的前序遍历
+     * 给定一个 N 叉树，返回其节点值的前序遍历。
+     * 方法1，递归调用
+     *
+     * @param root 树
+     * @return 返回遍历结果
+     */
+    public List<Integer> preorder(Node root) {
+        List<Integer> data = new ArrayList<>();
+        if (root == null) {
+            return data;
+        }
+        data.add(root.val);
+        for (Node child : root.children) {
+            data.addAll(preorder(child));
+        }
+        return data;
+    }
+
+    /**
+     * 589. N叉树的前序遍历
+     * 给定一个 N 叉树，返回其节点值的前序遍历。
+     * 方法2，迭代法
+     *
+     * @param root 树
+     * @return 返回遍历结果
+     */
+    public List<Integer> preorder1(Node root) {
+        List<Integer> data = new ArrayList<>();
+        if (root == null) {
+            return data;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            data.add(node.val);
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                stack.add(node.children.get(i));
+            }
+        }
+        return data;
+    }
+
+    private static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
     }
 }
