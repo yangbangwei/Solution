92b52d132a88302d6de11d3d14dad6b4abe529cc
yangbangwei <754667850@qq.com>
2019-12-06 14:06:48 +0800

    709. 转换成小写字母 核心点： 第一，判断A-Z，+32变成小写。 第二，位运算|32转化成大写。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_05.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_05.java
@@ -70,4 +70,23 @@ public class Test_2019_12_05 {
         return res;
     }
 
+    /**
+     * 709. 转换成小写字母
+     * 实现函数 ToLowerCase()，该函数接收一个字符串参数 str，
+     * 并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+     *
+     * @param str 字符串
+     * @return 小写字符串
+     */
+    private static String toLowerCase(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (c > 64 && c < 91) {
+                sb.append(c + 32);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
 }
