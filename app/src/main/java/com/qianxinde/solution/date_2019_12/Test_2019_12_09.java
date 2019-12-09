c469ea5d78d5a1c0d553d17c68211d32a58032db
yangbangwei <754667850@qq.com>
2019-12-09 14:40:45 +0800

    824. 山羊拉丁文 核心点： 第一，根据index添加‘a’的个数。非元音判断。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_09.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_09.java
@@ -19,6 +19,8 @@ public class Test_2019_12_09 {
                 "Hello", "Alaska", "Dad", "Peace"
         };
         System.out.println(Arrays.toString(findWords(words)));
+
+        System.out.println((toGoatLatin("The quick brown fox jumped over the lazy dog")));
     }
 
     /**
@@ -190,4 +192,46 @@ public class Test_2019_12_09 {
         return A;
     }
 
+    /**
+     * 824. 山羊拉丁文
+     * 给定一个由空格分割单词的句子 S。每个单词只包含大写或小写字母。
+     * 我们要将句子转换为 “Goat Latin”（一种类似于 猪拉丁文 - Pig Latin 的虚构语言）。
+     * <p>
+     * 山羊拉丁文的规则如下：
+     * 如果单词以元音开头（a, e, i, o, u），在单词后添加"ma"。
+     * 例如，单词"apple"变为"applema"。
+     * 如果单词以辅音字母开头（即非元音字母），移除第一个字符并将它放到末尾，之后再添加"ma"。
+     * 例如，单词"goat"变为"oatgma"。
+     * 根据单词在句子中的索引，在单词最后添加与索引相同数量的字母'a'，索引从1开始。
+     * 例如，在第一个单词后添加"a"，在第二个单词后添加"aa"，以此类推。
+     *
+     * @param S 单词句子
+     * @return 山羊拉丁文
+     */
+    private static String toGoatLatin(String S) {
+        String[] wrods = S.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < wrods.length; i++) {
+            String wrod = wrods[i];
+            if (!wrod.startsWith("a")
+                    && !wrod.startsWith("e")
+                    && !wrod.startsWith("i")
+                    && !wrod.startsWith("o")
+                    && !wrod.startsWith("u")
+                    && !wrod.startsWith("A")
+                    && !wrod.startsWith("E")
+                    && !wrod.startsWith("I")
+                    && !wrod.startsWith("O")
+                    && !wrod.startsWith("U")) {
+                wrod = wrod.substring(1) + wrod.substring(0, 1);
+            }
+            wrod += "ma";
+            StringBuilder sb = new StringBuilder(wrod);
+            for (int j = 0; j <= i; j++) {
+                sb.append("a");
+            }
+            stringBuilder.append(sb.toString()).append(" ");
+        }
+        return stringBuilder.toString().trim();
+    }
 }
