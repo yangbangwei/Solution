c46fff7f14ca5fdfbb0fb81d0985ead0400d7f49
yangbangwei <754667850@qq.com>
2019-12-09 11:17:01 +0800

    700. 二叉搜索树中的搜索 核心点： 第一，完全二叉树，左子树值小于右子树

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_09.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_09.java
@@ -137,4 +137,36 @@ public class Test_2019_12_09 {
         }
         return set.size();
     }
+
+    /**
+     * 700. 二叉搜索树中的搜索
+     * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。
+     * 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+     *
+     * @param root 二叉树
+     * @param val  值
+     * @return 返回根节点
+     */
+    private static TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 }
