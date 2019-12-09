cd2f9738281822b15212ae2e471cd64594789ffe
yangbangwei <754667850@qq.com>
2019-12-09 10:58:41 +0800

    804. 唯一摩尔斯密码词 核心点： 第一，使用set过滤同样的单词。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_09.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_09.java
@@ -2,7 +2,9 @@ package com.qianxinde.solution.date_2019_12;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author :yangbw
@@ -106,5 +108,33 @@ public class Test_2019_12_09 {
             ans[i - 1] = "FizzBuzz";
         }
         return Arrays.asList(ans);
+    }
+
+    /**
+     * 804. 唯一摩尔斯密码词
+     * 国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串，
+     *  比如: "a" 对应 ".-", "b" 对应 "-...", "c" 对应 "-.-.", 等等。
+     * 给定一个单词列表，每个单词可以写成每个字母对应摩尔斯密码的组合。例如，"cab"
+     * 可以写成 "-.-..--..."，(即 "-.-." + "-..." + ".-"字符串的结合)。我们将这样一个连接过程称作单词翻译。
+     * 返回我们可以获得所有词不同单词翻译的数量。
+     *
+     * @param words 单词列表
+     * @return 不同的单词数量
+     */
+    private static int uniqueMorseRepresentations(String[] words) {
+        String[] key = {".-", "-...", "-.-.", "-..", ".", "..-.",
+                "--.", "....", "..", ".---", "-.-", ".-..", "--",
+                "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+                "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                int i = c - 'a';
+                sb.append(key[i]);
+            }
+            set.add(sb.toString());
+        }
+        return set.size();
     }
 }
