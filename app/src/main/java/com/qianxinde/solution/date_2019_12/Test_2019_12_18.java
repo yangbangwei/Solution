cb027d88c6ccafd5515675b3d60a44a1275f3d43
yangbangwei <754667850@qq.com>
2019-12-18 10:44:11 +0800

    1025. 除数博弈 核心点： 1.每次都取1，实则为N%2==0。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_18.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_18.java
@@ -10,6 +10,8 @@ public class Test_2019_12_18 {
 
     public static void main(String[] args) {
         System.out.println(heightChecker1(new int[]{13, 13, 14, 12, 1, 3}));
+
+        System.out.println(divisorGame(3));
     }
 
     /**
@@ -55,5 +57,20 @@ public class Test_2019_12_18 {
             }
         }
         return count;
+    }
+
+    /**
+     * 1025. 除数博弈
+     * 爱丽丝和鲍勃一起玩游戏，他们轮流行动。爱丽丝先手开局。
+     * 最初，黑板上有一个数字 N 。在每个玩家的回合，玩家需要执行以下操作：
+     * 选出任一 x，满足 0 < x < N 且 N % x == 0 。
+     * 用 N - x 替换黑板上的数字 N 。
+     * 如果玩家无法执行这些操作，就会输掉游戏。
+     * 只有在爱丽丝在游戏中取得胜利时才返回 True，否则返回 false。假设两个玩家都以最佳状态参与游戏。
+     *
+     * @return 爱丽丝的输赢情况
+     */
+    private static boolean divisorGame(int N) {
+        return N % 2 == 0;
     }
 }
