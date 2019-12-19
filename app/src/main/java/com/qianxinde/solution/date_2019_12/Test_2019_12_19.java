b0d7f3228c6212e120f7dd58e85e59b1c21b99e6
yangbangwei <754667850@qq.com>
2019-12-19 14:17:46 +0800

    1089. 复写零 核心点： 1.插入后排序。 2.利用队列额外储存。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_19.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_19.java
@@ -10,6 +10,8 @@ public class Test_2019_12_19 {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(sortArrayByParity(new int[]{0, 2})));
+
+        System.out.println(Arrays.toString(duplicateZeros(new int[]{1, 0, 2, 3, 0, 4, 5, 0})));
     }
 
     /**
@@ -111,5 +113,26 @@ public class Test_2019_12_19 {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * 1089. 复写零
+     * 给你一个长度固定的整数数组 arr，请你将该数组中出现的每个零都复写一遍，并将其余的元素向右平移。
+     * 注意：请不要在超过该数组长度的位置写入元素。
+     * 要求：请对输入的数组 就地 进行上述修改，不要从函数返回任何东西。
+     *
+     * @param arr 整数数组
+     */
+    private static int[] duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == 0) {
+                if (arr.length - 1 - i + 1 >= 0) {
+                    System.arraycopy(arr, i + 1, arr, i + 2, arr.length - 1 - i + 1);
+                }
+                arr[i + 1] = 0;
+                i++;
+            }
+        }
+        return arr;
     }
 }
