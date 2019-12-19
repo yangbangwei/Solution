460c02b6e3323bbf70d8a813a20aea9d28c5427f
yangbangwei <754667850@qq.com>
2019-12-19 15:38:44 +0800

    1160. 拼写单词 核心点： 1.统计所有字母表的字母次数，然后判断是否支持拼写。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_19.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_19.java
@@ -169,4 +169,40 @@ public class Test_2019_12_19 {
         }
         return lastStoneWeight(newStones);
     }
+
+    /**
+     * 1160. 拼写单词
+     * 给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
+     * 假如你可以用 chars 中的『字母』（字符）拼写出 words 中的某个『单词』（字符串），
+     * 那么我们就认为你掌握了这个单词。
+     * 注意：每次拼写时，chars 中的每个字母都只能用一次。
+     * 返回词汇表 words 中你掌握的所有单词的 长度之和
+     *
+     * @param words 词汇表
+     * @param chars 字母表
+     * @return 掌握单词长度
+     */
+    private static int countCharacters(String[] words, String chars) {
+        int[] bucket = new int[26];
+        for (char c : chars.toCharArray()) {
+            bucket[c - 'a']++;
+        }
+        int ans = 0;
+        for (String word : words) {
+            int[] temp = new int[26];
+            System.arraycopy(bucket, 0, temp, 0, temp.length);
+            boolean isAdd = true;
+            for (char c : word.toCharArray()) {
+                if (temp[c - 'a'] > 0) {
+                    temp[c - 'a']--;
+                } else {
+                    isAdd = false;
+                }
+            }
+            if (isAdd) {
+                ans += word.length();
+            }
+        }
+        return ans;
+    }
 }
