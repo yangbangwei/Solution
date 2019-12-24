36370aa41d1ac2c00403650fa17396c93012d62d
yangbangwei <754667850@qq.com>
2019-12-24 16:03:44 +0800

    455. 分发饼干 核心点： 1.贪心算法。排序后，一一对比，是否符合。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_24.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_24.java
@@ -248,4 +248,28 @@ public class Test_2019_12_24 {
         }
         return ans;
     }
+
+    /**
+     * 455. 分发饼干
+     * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+     * 对每个孩子 i ，都有一个胃口值 gi ，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j ，
+     * 都有一个尺寸 sj 。如果 sj >= gi ，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。
+     * 你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+     *
+     * @param g 胃口值
+     * @param s 饼干
+     * @return 最大数值
+     */
+    private static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans = 0;
+        for (int i = 0, j = 0; i < g.length && j < s.length; j++) {
+            if (g[i] <= s[j]) {
+                ans++;
+                i++;
+            }
+        }
+        return ans;
+    }
 }
