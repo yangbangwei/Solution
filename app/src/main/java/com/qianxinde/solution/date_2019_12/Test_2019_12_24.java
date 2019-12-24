d85eba46bb10f6a0d34e033abef4cd6ad2461038
yangbangwei <754667850@qq.com>
2019-12-24 15:37:41 +0800

    409. 最长回文串 核心点： 1.贪心算法，count/2*2，并且判断‘aaa’的情况，再加1。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_24.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_24.java
@@ -223,4 +223,29 @@ public class Test_2019_12_24 {
         }
         return false;
     }
+
+    /**
+     * 409. 最长回文串
+     * 给定一个包含大写字母和小写字母的字符串，找到通过这些字母构造成的最长的回文串。
+     * 在构造过程中，请注意区分大小写。比如 "Aa" 不能当做一个回文字符串。
+     * 注意:
+     * 假设字符串的长度不会超过 1010。
+     *
+     * @param s 字符串
+     * @return 回文的长度
+     */
+    private static int longestPalindrome(String s) {
+        int[] nums = new int[126];
+        for (char c : s.toCharArray()) {
+            nums[c]++;
+        }
+        int ans = 0;
+        for (int num : nums) {
+            ans += num / 2 * 2;
+            if (num % 2 == 1 && ans % 2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
 }
