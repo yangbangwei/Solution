dbb9f85ef1f333ee915f228ee351af1b5682afe9
yangbangwei <754667850@qq.com>
2019-12-26 10:32:46 +0800

    1128. 等价多米诺骨牌对的数量 核心点： 1.创建nums[100]的数组，保存所有x,y的值。 2.遍历nums，累加n*(n-1)/2的所有的情况。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_26.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_26.java
@@ -30,6 +30,32 @@ public class Test_2019_12_26 {
         }
         return ans;
     }
+
+    /**
+     * 1128. 等价多米诺骨牌对的数量
+     * 给你一个由一些多米诺骨牌组成的列表 dominoes。
+     * 如果其中某一张多米诺骨牌可以通过旋转 0 度或 180 度得到另一张多米诺骨牌，我们就认为这两张牌是等价的。
+     * 形式上，dominoes[i] = [a, b] 和 dominoes[j] = [c, d] 等价的前提是 a==c 且 b==d，或是 a==d 且 b==c。
+     * 在 0 <= i < j < dominoes.length 的前提下，找出满足 dominoes[i] 和 dominoes[j] 
+     * 等价的骨牌对 (i, j) 的数量。
+     *
+     * @param dominoes 多米诺牌
+     * @return 等价的数量
+     */
+    private static int numEquivDominoPairs(int[][] dominoes) {
+        int[] nums = new int[100];
+        for (int[] dominoe : dominoes) {
+            int x = dominoe[0];
+            int y = dominoe[1];
+            int k = x > y ? x * 10 + y : y * 10 + x;
+            nums[k]++;
+        }
+        int ans = 0;
+        for (int num : nums) {
+            ans += num * (num - 1) / 2;
+        }
+        return ans;
+    }
 }
 
 
