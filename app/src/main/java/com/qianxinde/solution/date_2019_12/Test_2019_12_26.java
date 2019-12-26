7e0b62c71f9d861f1b5c94d588989d93a6bae4bb
yangbangwei <754667850@qq.com>
2019-12-26 16:39:59 +0800

    1078. Bigram 分词 核心点： 1.判断 words[i - 2].equals(first) && words[i -1].equals(second)

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_26.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_26.java
@@ -1,5 +1,9 @@
 package com.qianxinde.solution.date_2019_12;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author :yangbw
  * @date :2019-12-26
@@ -8,6 +12,9 @@ public class Test_2019_12_26 {
 
     public static void main(String[] args) {
         System.out.println(dayOfYear("2016-02-29"));
+
+        System.out.println(Arrays.toString(findOcurrences("alice is a good girl she is a good student"
+                , "a", "good")));
     }
 
     /**
@@ -123,6 +130,27 @@ public class Test_2019_12_26 {
             }
         }
         return monday[month - 1] + day;
+    }
+
+    /**
+     * 1078. Bigram 分词
+     * 给出第一个词 first 和第二个词 second，考虑在某些文本 text 中可能以 "first second third" 形式出现的情况，其中 second 紧随 first 出现，third 紧随 second 出现。
+     * 对于每种这样的情况，将第三个词 "third" 添加到答案中，并返回答案。
+     *
+     * @param text   文字
+     * @param first  第一个词
+     * @param second 第二个词
+     * @return 第三个词
+     */
+    private static String[] findOcurrences(String text, String first, String second) {
+        List<String> data = new ArrayList<>();
+        String[] words = text.split(" ");
+        for (int i = 2; i < words.length; i++) {
+            if (words[i - 2].equals(first) && words[i - 1].equals(second)) {
+                data.add(words[i]);
+            }
+        }
+        return data.toArray(new String[0]);
     }
 }
 
