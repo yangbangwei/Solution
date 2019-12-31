32ed17a61f51939ff8beb9b09adb77425a12d32b
yangbangwei <754667850@qq.com>
2019-12-31 16:27:17 +0800

    1299. 将每个元素替换为右侧最大元素 核心点： 1.从末尾开始算，max从后往前累积判断。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_31.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_31.java
@@ -1,5 +1,7 @@
 package com.qianxinde.solution.date_2019_12;
 
+import java.util.Arrays;
+
 /**
  * @author :yangbw
  * @date :2019-12-27
@@ -8,6 +10,8 @@ public class Test_2019_12_31 {
 
     public static void main(String[] args) {
         System.out.println(dayOfTheWeek(31, 8, 2019));
+
+        System.out.println(Arrays.toString(replaceElements(new int[]{17, 18, 5, 4, 6, 1})));
     }
 
     /**
@@ -64,6 +68,27 @@ public class Test_2019_12_31 {
             ans[n - i - 1] = -(i + 1);
         }
         return ans;
+    }
+
+    /**
+     * 1299. 将每个元素替换为右侧最大元素
+     * 给你一个数组 arr ，请你将每个元素用它右边最大的元素替换，如果是最后一个元素，用 -1 替换。
+     * 完成所有替换操作后，请你返回这个数组。
+     *
+     * @param arr 数组
+     * @return 替换后的数组
+     */
+    private static int[] replaceElements(int[] arr) {
+        int max = arr[arr.length - 1];
+        arr[arr.length - 1] = -1;
+        for (int i = arr.length - 2; i > 0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return arr;
     }
 }
 
