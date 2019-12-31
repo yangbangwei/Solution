18f8801f3f75f3ed1dcbb9b3f45d421cb0cf57a9
yangbangwei <754667850@qq.com>
2019-12-31 17:24:43 +0800

    1260. 二维网格迁移 核心点： 1.创建num[n*m]的数组，一次迁移k位。

--- app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_31.java
+++ app/src/main/java/com/qianxinde/solution/date_2019_12/Test_2019_12_31.java
@@ -1,6 +1,8 @@
 package com.qianxinde.solution.date_2019_12;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author :yangbw
@@ -89,6 +91,41 @@ public class Test_2019_12_31 {
             }
         }
         return arr;
+    }
+
+    /**
+     * 1260. 二维网格迁移
+     * 给你一个 n 行 m 列的二维网格 grid 和一个整数 k。你需要将 grid 迁移 k 次。
+     * 每次「迁移」操作将会引发下述活动：
+     * 位于 grid[i][j] 的元素将会移动到 grid[i][j + 1]。
+     * 位于 grid[i][m - 1] 的元素将会移动到 grid[i + 1][0]。
+     * 位于 grid[n - 1][m - 1] 的元素将会移动到 grid[0][0]。
+     * 请你返回 k 次迁移操作后最终得到的 二维网格。
+     *
+     * @param grid 二维数组
+     * @param k    迁移k次
+     * @return 迁移后的结果
+     */
+    private static List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] temp = new int[n * m];
+        k %= temp.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                temp[(i * m + j + k) % temp.length] = grid[i][j];
+            }
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> t = new ArrayList<>();
+        for (int i = 0; i < temp.length; i++) {
+            t.add(temp[i]);
+            if ((i + 1) % m == 0) {
+                res.add(t);
+                t = new ArrayList<>();
+            }
+        }
+        return res;
     }
 }
 
