1c175c533e6da77ff0bf5c3b41c0562c5d057610
yangbangwei <754667850@qq.com>
2020-01-02 10:30:35 +0800

    1184. 公交站间的距离 核心点： 1.起始点，结束节点的大小关系。 2.从起始点到结束点。 3.从0到左加上从length到结束点。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_02.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_02.java
@@ -9,7 +9,8 @@ import java.util.Arrays;
 public class Test_2020_01_02 {
 
     public static void main(String[] args) {
-
+        int[] distance = {7, 10, 1, 12, 11, 14, 5, 0};
+        System.out.println(distanceBetweenBusStops(distance, 7, 2));
     }
 
     /**
@@ -59,5 +60,36 @@ public class Test_2020_01_02 {
             }
         }
         return ans;
+    }
+
+    /**
+     * 1184. 公交站间的距离
+     * 环形公交路线上有 n 个站，按次序从 0 到 n - 1 进行编号。我们已知每一对相邻公交站之间的距离，
+     * distance[i] 表示编号为 i 的车站和编号为 (i + 1) % n 的车站之间的距离。
+     * 环线上的公交车都可以按顺时针和逆时针的方向行驶。
+     * 返回乘客从出发点 start 到目的地 destination 之间的最短距离。
+     *
+     * @param distance    距离
+     * @param start       开始
+     * @param destination 结束
+     * @return 最短距离
+     */
+    private static int distanceBetweenBusStops(int[] distance, int start, int destination) {
+        int left = 0, right = 0;
+        if (start > destination) {
+            int temp = start;
+            start = destination;
+            destination = temp;
+        }
+        for (int i = start; i < destination; i++) {
+            right += distance[i];
+        }
+        for (int i = distance.length - 1; i >= destination; i--) {
+            left += distance[i];
+        }
+        for (int i = 0; i < start; i++) {
+            left += distance[i];
+        }
+        return left < right ? left : right;
     }
 }
