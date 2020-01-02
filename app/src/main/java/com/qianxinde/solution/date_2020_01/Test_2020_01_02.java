d9b2da39845e5a53cd4eb138a78d8f31a7cb9a63
yangbangwei <754667850@qq.com>
2020-01-02 15:10:03 +0800

    1071. 字符串的最大公因子 核心点： 1.(str1 + str2).equals(str2 + str1)

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_02.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_02.java
@@ -230,4 +230,25 @@ public class Test_2020_01_02 {
         }
         return ans;
     }
+
+    /**
+     * 1071. 字符串的最大公因子
+     * 对于字符串 S 和 T，只有在 S = T + ... + T（T 与自身连接 1 次或多次）时，我们才认定 “T 能除尽 S”。
+     * 返回字符串 X，要求满足 X 能除尽 str1 且 X 能除尽 str2。
+     *
+     * @param str1 字符串s
+     * @param str2 字符串t
+     * @return 最大公因子
+     */
+    private String gcdOfStrings(String str1, String str2) {
+        if ((str1 + str2).equals(str2 + str1)) {
+            for (int i = str1.length(); i > 0; i--) {
+                if (str1.length() % i == 0 && str2.length() % i == 0
+                        && (str1.substring(0, i) + str2).equals(str2 + str1.substring(0, i))) {
+                    return str1.substring(0, i);
+                }
+            }
+        }
+        return "";
+    }
 }
