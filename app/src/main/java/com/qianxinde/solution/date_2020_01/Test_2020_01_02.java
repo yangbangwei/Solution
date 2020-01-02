91355865de0158a99a0e4077c65fc2ed3e6ae052
yangbangwei <754667850@qq.com>
2020-01-02 15:44:40 +0800

    1018. 可被 5 整除的二进制前缀 核心点： 1.累加上一位剩余的值，再判断取余5等于0。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_02.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_02.java
@@ -18,6 +18,9 @@ public class Test_2020_01_02 {
         System.out.println(oddCells(2, 3, indices));
 
         System.out.println(Arrays.toString(distributeCandies(7, 4)));
+
+        System.out.println(prefixesDivBy5(new int[]{1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+                , 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1}));
     }
 
     /**
@@ -250,5 +253,24 @@ public class Test_2020_01_02 {
             }
         }
         return "";
+    }
+
+    /**
+     * 1018. 可被 5 整除的二进制前缀
+     * 给定由若干 0 和 1 组成的数组 A。我们定义 N_i：从 A[0] 到 A[i] 的第 i 个子数组被解释为一个二进制数
+     * （从最高有效位到最低有效位）。
+     * 返回布尔值列表 answer，只有当 N_i 可以被 5 整除时，答案 answer[i] 为 true，否则为 false。
+     *
+     * @param A 数组A
+     * @return 是否能被5整除
+     */
+    private static List<Boolean> prefixesDivBy5(int[] A) {
+        List<Boolean> ans = new ArrayList<>();
+        int mod = 0;
+        for (int i : A) {
+            mod = (2 * mod + i) % 5;
+            ans.add(mod % 5 == 0);
+        }
+        return ans;
     }
 }
