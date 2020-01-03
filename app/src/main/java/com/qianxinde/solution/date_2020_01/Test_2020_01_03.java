b5d306654de66259ab5fb7364b381cb2560cc0fa
yangbangwei <754667850@qq.com>
2020-01-03 10:50:54 +0800

    724. 寻找数组的中心索引 核心点： 1.统计总合，再统计左边值总和，判断leftSum == total -nums[i] - leftSum。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
@@ -7,7 +7,7 @@ package com.qianxinde.solution.date_2020_01;
 public class Test_2020_01_03 {
 
     public static void main(String[] args) {
-
+        System.out.println(pivotIndex(new int[]{-1, -1, -1, -1, -1, 0}));
     }
 
     /**
@@ -92,6 +92,32 @@ public class Test_2020_01_03 {
             }
         }
         return ' ';
+    }
+
+    /**
+     * 724. 寻找数组的中心索引
+     * 给定一个整数类型的数组 nums，请编写一个能够返回数组“中心索引”的方法。
+     * 我们是这样定义数组中心索引的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+     * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
+     *
+     * @param nums 数组
+     * @return 中心索引
+     */
+    private static int pivotIndex(int[] nums) {
+        if (nums.length > 2) {
+            int total = 0;
+            int leftSum = 0;
+            for (int num : nums) {
+                total += num;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                if (leftSum == total - nums[i] - leftSum) {
+                    return i;
+                }
+                leftSum += nums[i];
+            }
+        }
+        return -1;
     }
 }
 
