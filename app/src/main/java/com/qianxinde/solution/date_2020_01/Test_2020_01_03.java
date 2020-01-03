47888c9af3cce3a29910d30170c575dbd48829e9
yangbangwei <754667850@qq.com>
2020-01-03 14:37:43 +0800

    566. 重塑矩阵 核心点： 1.newNums[index / c][index % c] = nums[i][j];

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
@@ -1,6 +1,7 @@
 package com.qianxinde.solution.date_2020_01;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 
 /**
@@ -13,6 +14,9 @@ public class Test_2020_01_03 {
         System.out.println(pivotIndex(new int[]{-1, -1, -1, -1, -1, 0}));
 
         System.out.println(containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
+
+        int[][] nums = {{1, 2}, {3, 4}};
+        System.out.println(Arrays.deepToString(matrixReshape(nums, 1, 4)));
     }
 
     /**
@@ -149,6 +153,36 @@ public class Test_2020_01_03 {
             }
         }
         return false;
+    }
+
+    /**
+     * 566. 重塑矩阵
+     * 在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+     * 给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。
+     * 重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+     * 如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+     *
+     * @param nums 数组
+     * @param r    行
+     * @param c    列
+     * @return 重塑或原始矩阵
+     */
+    private static int[][] matrixReshape(int[][] nums, int r, int c) {
+        int row = nums[0].length;
+        int col = nums.length;
+        if (row * col == r * c) {
+            int[][] newNums = new int[r][c];
+            int index = 0;
+            for (int[] num : nums) {
+                for (int j = 0; j < row; j++) {
+                    newNums[index / c][index % c] = num[j];
+                    index++;
+                }
+            }
+            return newNums;
+        } else {
+            return nums;
+        }
     }
 }
 
