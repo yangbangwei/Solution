1a48fa3dfced09bebae09de2d0ec29ff576109fd
yangbangwei <754667850@qq.com>
2020-01-03 14:54:27 +0800

    561. 数组拆分 I 核心点： 1.排序后，奇数位相加。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
@@ -184,5 +184,22 @@ public class Test_2020_01_03 {
             return nums;
         }
     }
+
+    /**
+     * 561. 数组拆分 I
+     * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，
+     * 使得从1 到 n 的 min(ai, bi) 总和最大。
+     *
+     * @param nums 数组
+     * @return 两组min的总和
+     */
+    private int arrayPairSum(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
 }
 
