dd5dd9982eb5f349ae1f16f0378d267693399ea1
yangbangwei <754667850@qq.com>
2020-01-03 16:56:05 +0800

    888. 公平的糖果交换 核心点： 1.计算A，B总和，x =（B-A)/2计算出差值。 2.通过HashSet，取出符合num-x的数。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
@@ -3,6 +3,7 @@ package com.qianxinde.solution.date_2020_01;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 
@@ -249,6 +250,37 @@ public class Test_2020_01_03 {
         if (root.right != null) {
             dfs(data, s + "->" + root.val, root.right);
         }
+    }
+
+    /**
+     * 888. 公平的糖果交换
+     * 爱丽丝和鲍勃有不同大小的糖果棒：A[i] 是爱丽丝拥有的第 i 块糖的大小，B[j] 是鲍勃拥有的第 j 块糖的大小。
+     * 因为他们是朋友，所以他们想交换一个糖果棒，这样交换后，他们都有相同的糖果总量。
+     * （一个人拥有的糖果总量是他们拥有的糖果棒大小的总和。）
+     * 返回一个整数数组 ans，其中 ans[0] 是爱丽丝必须交换的糖果棒的大小，ans[1] 是 Bob 必须交换的糖果棒的大小。
+     * 如果有多个答案，你可以返回其中任何一个。保证答案存在。
+     *
+     * @param A 数组A
+     * @param B 数组B
+     * @return 需要交换的糖果
+     */
+    private int[] fairCandySwap(int[] A, int[] B) {
+        int sumA = 0, sumB = 0;
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i : A) {
+            sumA += i;
+        }
+        for (int i : B) {
+            sumB += i;
+            hashSet.add(i);
+        }
+        int x = (sumB - sumA) / 2;
+        for (int i : A) {
+            if (hashSet.contains(i + x)) {
+                return new int[]{i, x + i};
+            }
+        }
+        return null;
     }
 }
 
