1bdb1c7921db2cc15ccce868482a0603c3f1a919
yangbangwei <754667850@qq.com>
2020-01-03 10:10:37 +0800

    744. 寻找比目标字母大的最小字母 核心点： 1.判断字母是否大于目标字母。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_03.java
@@ -75,5 +75,23 @@ public class Test_2020_01_03 {
         }
         return 0;
     }
+
+    /**
+     * 744. 寻找比目标字母大的最小字母
+     * 给定一个只包含小写字母的有序数组letters 和一个目标字母 target，寻找有序数组里面比目标字母大的最小字母。
+     * 数组里字母的顺序是循环的。举个例子，如果目标字母target = 'z' 并且有序数组为 letters = ['a', 'b']，则答案返回 'a'。
+     *
+     * @param letters 有序数组
+     * @param target  目标字母
+     * @return 最小的字母
+     */
+    private char nextGreatestLetter(char[] letters, char target) {
+        for (char letter : letters) {
+            if (target < letter) {
+                return letter;
+            }
+        }
+        return ' ';
+    }
 }
 
