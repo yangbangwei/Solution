d974fc5709dfc164f0d3cdbecc5b1b69c603a70e
yangbangwei <754667850@qq.com>
2020-01-06 15:16:11 +0800

    605. 种花问题 核心点： 1. 注意左右边界。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
@@ -16,6 +16,8 @@ public class Test_2020_01_06 {
         System.out.println(isHappy(19));
 
         System.out.println(toHex(26));
+
+        System.out.println(canPlaceFlowers(new int[]{0, 0, 1, 0, 0}, 1));
     }
 
     /**
@@ -175,6 +177,30 @@ public class Test_2020_01_06 {
             num >>>= 4;
         }
         return sb.toString().isEmpty() ? "0" : sb.reverse().toString();
+    }
+
+    /**
+     * 605. 种花问题
+     * 假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，
+     * 花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
+     * 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），
+     * 和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
+     *
+     * @param flowerbed 花坛
+     * @param n         花朵数
+     * @return 是否可行
+     */
+    private static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0
+                    && (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                count++;
+                flowerbed[i] = 1;
+            }
+        }
+        return n <= count;
     }
 }
 
