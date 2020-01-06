47dd2e75d0e0847dbd73817d90b9b04ad5c77265
yangbangwei <754667850@qq.com>
2020-01-06 10:39:43 +0800

    198. 打家劫舍 核心点： 1. 动态规划， int temp = currMax; currMax = Math.max(prevMax + num, currMax); prevMax = temp;

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
@@ -8,6 +8,8 @@ public class Test_2020_01_06 {
 
     public static void main(String[] args) {
         System.out.println(longestCommonPrefix(new String[]{"dog", "doracecar", "car"}));
+
+        System.out.println(rob(new int[]{2, 7, 9, 3, 1}));
     }
 
     /**
@@ -59,10 +61,34 @@ public class Test_2020_01_06 {
      */
     private static int reverseBits(int n) {
         int ans = 0;
-        for (int i = 31; i >= 0; i--) {
-            ans = ans | ((n >> (31 - i) & 1)) << i;
+        int count = 0;
+        while (count < 32) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+            count++;
         }
         return ans;
+    }
+
+    /**
+     * 198. 打家劫舍
+     * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
+     * 影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+     *
+     * @param nums 整数数组
+     * @return 最高金额
+     */
+    private static int rob(int[] nums) {
+        int prevMax = 0;
+        int currMax = 0;
+        for (int num : nums) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + num, currMax);
+            prevMax = temp;
+        }
+        return currMax;
     }
 }
 
