b702a597b66df342269584a5d07d6a04ce44a616
yangbangwei <754667850@qq.com>
2020-01-06 09:51:48 +0800

    122. 买卖股票的最佳时机 II 核心点： 1.最低价和最高价之差。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
@@ -30,5 +30,24 @@ public class Test_2020_01_06 {
         }
         return s;
     }
+
+    /**
+     * 122. 买卖股票的最佳时机 II
+     * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+     * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+     * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     *
+     * @param prices 股票价格
+     * @return 最大利润
+     */
+    private int maxProfit(int[] prices) {
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                max += prices[i] - prices[i - 1];
+            }
+        }
+        return max;
+    }
 }
 
