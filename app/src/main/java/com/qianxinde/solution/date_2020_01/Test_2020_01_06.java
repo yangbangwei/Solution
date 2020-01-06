1e8d8bbc464db2e7b26317ce312f09610f3e8496
yangbangwei <754667850@qq.com>
2020-01-06 14:37:25 +0800

    405. 数字转换为十六进制数 核心点： 1. num&15，左移4位。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
@@ -14,6 +14,8 @@ public class Test_2020_01_06 {
         System.out.println(isSubsequence("abc", "ahbgdc"));
 
         System.out.println(isHappy(19));
+
+        System.out.println(toHex(26));
     }
 
     /**
@@ -152,5 +154,27 @@ public class Test_2020_01_06 {
         return sum;
     }
 
+    /**
+     * 405. 数字转换为十六进制数
+     * 给定一个整数，编写一个算法将这个数转换为十六进制数。对于负整数，我们通常使用 补码运算 方法。
+     * 注意:
+     * 十六进制中所有字母(a-f)都必须是小写。
+     * 十六进制字符串中不能包含多余的前导零。如果要转化的数为0，那么以单个字符'0'来表示；对于其他情况，
+     * 十六进制字符串中的第一个字符将不会是0字符。 
+     * 给定的数确保在32位有符号整数范围内。
+     * 不能使用任何由库提供的将数字直接转换或格式化为十六进制的方法。
+     *
+     * @param num 整数
+     * @return 转成16进制
+     */
+    private static String toHex(int num) {
+        char[] hex = "0123456789abcdef".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.append(hex[num & 15]);
+            num >>>= 4;
+        }
+        return sb.toString().isEmpty() ? "0" : sb.reverse().toString();
+    }
 }
 
