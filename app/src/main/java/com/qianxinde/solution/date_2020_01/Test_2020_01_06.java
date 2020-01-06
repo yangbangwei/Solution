7cbf4111178974e2ea776b1429a981c3d8b626e4
yangbangwei <754667850@qq.com>
2020-01-06 10:55:41 +0800

    392. 判断子序列 核心点： 1. t.indexOf(c, idx + 1)

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_06.java
@@ -10,6 +10,8 @@ public class Test_2020_01_06 {
         System.out.println(longestCommonPrefix(new String[]{"dog", "doracecar", "car"}));
 
         System.out.println(rob(new int[]{2, 7, 9, 3, 1}));
+
+        System.out.println(isSubsequence("abc", "ahbgdc"));
     }
 
     /**
@@ -89,6 +91,27 @@ public class Test_2020_01_06 {
             prevMax = temp;
         }
         return currMax;
+    }
+
+    /**
+     * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+     * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+     * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。
+     * （例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+     *
+     * @param s 字符串
+     * @param t 字符串
+     * @return s是否为t的子序列
+     */
+    private static boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for (char aChar : s.toCharArray()) {
+            index = t.indexOf(aChar, index + 1);
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
