bd17f88526f7b4778375747d05b702ddda9976d5
yangbangwei <754667850@qq.com>
2020-01-07 16:05:55 +0800

    929. 独特的电子邮件地址 核心点： 1.根据规则遍历后，Set判断个数。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_07.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_07.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -20,6 +21,8 @@ public class Test_2020_01_07 {
                 "Burger King", "KFC"}, new String[]{"KFC", "Shogun", "Burger King"})));
 
         System.out.println(dominantIndex(new int[]{0, 0, 0, 1}));
+
+        System.out.println(numUniqueEmails(new String[]{"test.email+alex@leetcode.com", "test.email.leet+alex@code.com"}));
     }
 
     /**
@@ -127,5 +130,34 @@ public class Test_2020_01_07 {
             }
         }
         return max;
+    }
+
+    /**
+     * 929. 独特的电子邮件地址
+     * 每封电子邮件都由一个本地名称和一个域名组成，以 @ 符号分隔。
+     * 例如，在 alice@leetcode.com中， alice 是本地名称，而 leetcode.com 是域名。
+     * 除了小写字母，这些电子邮件还可能包含 '.' 或 '+'。
+     * 如果在电子邮件地址的本地名称部分中的某些字符之间添加句点（'.'），
+     * 则发往那里的邮件将会转发到本地名称中没有点的同一地址。
+     * 例如，"alice.z@leetcode.com” 和 “alicez@leetcode.com” 会转发到同一电子邮件地址。 （请注意，此规则不适用于域名。）
+     * 如果在本地名称中添加加号（'+'），则会忽略第一个加号后面的所有内容。
+     * 这允许过滤某些电子邮件，例如 m.y+name@email.com 将转发到 my@email.com。 （同样，此规则不适用于域名。）
+     * 可以同时使用这两个规则。
+     * 给定电子邮件列表 emails，我们会向列表中的每个地址发送一封电子邮件。实际收到邮件的不同地址有多少？
+     *
+     * @param emails 邮件地址
+     * @return 不同的地址数
+     */
+    private static int numUniqueEmails(String[] emails) {
+        HashSet<String> hashSet = new HashSet<>();
+        for (String email : emails) {
+            String[] words = email.split("@");
+            String s = words[0].replace(".", "");
+            if (s.contains("+")) {
+                s = s.substring(0, s.indexOf("+"));
+            }
+            hashSet.add(s.concat("@").concat(words[1]));
+        }
+        return hashSet.size();
     }
 }
