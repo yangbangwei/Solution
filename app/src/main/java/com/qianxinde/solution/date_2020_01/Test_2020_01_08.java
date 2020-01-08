56b74d910c573b1ebcefefb5ce60b43baa66e087
yangbangwei <754667850@qq.com>
2020-01-08 16:09:20 +0800

    686. 重复叠加字符串匹配 核心点： 1.重复到A,B字符长度相同。判断是否包含。 2.再循环一次，（头尾相加的情况），是否包含。 3.否则返回-1。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_08.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_08.java
@@ -188,5 +188,24 @@ public class Test_2020_01_08 {
         return 0;
     }
 
+    /**
+     * 686. 重复叠加字符串匹配
+     * 给定两个字符串 A 和 B, 寻找重复叠加字符串A的最小次数，使得字符串B成为叠加后的字符串A的子串，如果不存在则返回 -1。
+     * 举个例子，A = "abcd"，B = "cdabcdab"。
+     * 答案为 3， 因为 A 重复叠加三遍后为 “abcdabcdabcd”，此时 B 是其子串；A 重复叠加两遍后为"abcdabcd"，B 并不是其子串。
+     *
+     * @param A 字符串A
+     * @param B 字符串B
+     * @return 重复次数
+     */
+    private int repeatedStringMatch(String A, String B) {
+        StringBuilder sb = new StringBuilder(A);
+        int sum = 1;
+        while (sb.length() < B.length()) {
+            sb.append(A);
+            sum++;
+        }
+        return sb.lastIndexOf(B) >= 0 ? sum : (sb.append(A).lastIndexOf(B) >= 0 ? sum + 1 : -1);
+    }
 
 }
