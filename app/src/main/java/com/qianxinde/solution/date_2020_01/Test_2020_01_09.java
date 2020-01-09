5652b084a1d88cf2bf30aa44a800d9766398994e
yangbangwei <754667850@qq.com>
2020-01-09 11:04:28 +0800

    1009. 十进制整数的反码 核心点： 1.遍历后，取反相加。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_09.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_09.java
@@ -12,6 +12,8 @@ public class Test_2020_01_09 {
         System.out.println(hasAlternatingBits(11));
 
         System.out.println(countPrimeSetBits(10, 15));
+
+        System.out.println(bitwiseComplement(7));
     }
 
     /**
@@ -87,5 +89,32 @@ public class Test_2020_01_09 {
             }
         }
         return true;
+    }
+
+    /**
+     * 1009. 十进制整数的反码
+     * 每个非负整数 N 都有其二进制表示。例如， 5 可以被表示为二进制 "101"，11 可以用二进制 "1011" 表示，
+     * 依此类推。注意，除 N = 0 外，任何二进制表示中都不含前导零。
+     * 二进制的反码表示是将每个 1 改为 0 且每个 0 变为 1。例如，二进制数 "101" 的二进制反码为 "010"。
+     * 给定十进制数 N，返回其二进制表示的反码所对应的十进制整数。
+     *
+     * @param N 整数
+     * @return 反码后的值
+     */
+    private static int bitwiseComplement(int N) {
+        if (N == 0) {
+            return 1;
+        }
+        int ans = 0;
+        int i = 0;
+        while (N > 0) {
+            int temp = N % 2;
+            if (temp == 0) {
+                ans += Math.pow(2, i);
+            }
+            i++;
+            N /= 2;
+        }
+        return ans;
     }
 }
