2191508638fc3216aa3294f484fb4ff4ed879472
yangbangwei <754667850@qq.com>
2020-01-09 16:53:19 +0800

    645. 错误的集合 核心点： 1.hashmap统计。 2.将所有的值存入数组，然后判断次数为0，2的值。 3.排序后遍历判断，nums[i] > nums[i - 1] + 1。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_09.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_09.java
@@ -300,12 +300,40 @@ public class Test_2020_01_09 {
      * @param nums 整数数组
      * @return 最小移动步数
      */
-    public int minMoves(int[] nums) {
+    private int minMoves(int[] nums) {
         Arrays.sort(nums);
         int ans = 0;
         for (int i = nums.length - 1; i > 0; i--) {
             ans += nums[i] - nums[0];
         }
         return ans;
+    }
+
+    /**
+     * 645. 错误的集合
+     * 集合 S 包含从1到 n 的整数。不幸的是，因为数据错误，
+     * 导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重复。
+     * 给定一个数组 nums 代表了集合 S 发生错误后的结果。
+     * 你的任务是首先寻找到重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+     *
+     * @param nums 数组
+     * @return 重复，以及丢失整数
+     */
+    private int[] findErrorNums(int[] nums) {
+        int[] count = new int[nums.length + 1];
+
+        for (int i : nums) {
+            count[i]++;
+        }
+
+        int[] res = new int[2];
+        for (int i = 1; i < count.length; i++) {
+            if (count[i] == 0) {
+                res[1] = i;
+            } else if (count[i] == 2) {
+                res[0] = i;
+            }
+        }
+        return res;
     }
 }
