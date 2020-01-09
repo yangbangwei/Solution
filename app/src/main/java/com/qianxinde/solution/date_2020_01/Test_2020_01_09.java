e57725165cae469ab7ac287b28c0e3da90d629a3
yangbangwei <754667850@qq.com>
2020-01-09 16:11:07 +0800

    717. 1比特与2比特字符 核心点： 1.根据i += bits[i] + 1循环，最后判断i==bits.length-1。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_09.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_09.java
@@ -27,6 +27,8 @@ public class Test_2020_01_09 {
         System.out.println(maxDistToClosest(new int[]{0, 1}));
 
         System.out.println(largeGroupPositions("abcdddeeeeaabbbcd"));
+
+        System.out.println(isOneBitCharacter(new int[]{1, 1, 1, 0}));
     }
 
     /**
@@ -272,5 +274,21 @@ public class Test_2020_01_09 {
             }
         }
         return ans;
+    }
+
+    /**
+     * 717. 1比特与2比特字符
+     * 有两种特殊字符。第一种字符可以用一比特0来表示。第二种字符可以用两比特(10 或 11)来表示。
+     * 现给一个由若干比特组成的字符串。问最后一个字符是否必定为一个一比特字符。给定的字符串总是由0结束。
+     *
+     * @param bits 数组
+     * @return 是否以0结尾
+     */
+    private static boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i] + 1;
+        }
+        return i == bits.length - 1;
     }
 }
