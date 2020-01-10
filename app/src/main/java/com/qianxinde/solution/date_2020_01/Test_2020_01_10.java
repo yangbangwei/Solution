b773488e1689b4c597cd12818e3e326a757ff40e
yangbangwei <754667850@qq.com>
2020-01-10 17:25:50 +0800

    914. 卡牌分组 核心点： 1.统计所有出现次数。 2.计算最大公约数。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_10.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_10.java
@@ -22,6 +22,8 @@ public class Test_2020_01_10 {
                 new String[]{"step", "steps", "stripe", "stepple"}));
 
         System.out.println(numMagicSquaresInside(new int[][]{{1, 1, 1}, {4, 5, 6}, {9, 9, 9}}));
+
+        System.out.println(hasGroupsSizeX(new int[]{1, 1, 2, 2, 2, 2}));
     }
 
     /**
@@ -287,5 +289,45 @@ public class Test_2020_01_10 {
             }
         }
         return count;
+    }
+
+    /**
+     * 914. 卡牌分组
+     * 给定一副牌，每张牌上都写着一个整数。
+     * 此时，你需要选定一个数字 X，使我们可以将整副牌按下述规则分成 1 组或更多组：
+     * 每组都有 X 张牌。
+     * 组内所有的牌上都写着相同的整数。
+     * 仅当你可选的 X >= 2 时返回 true。
+     *
+     * @param deck 牌的数组
+     * @return 公约数
+     */
+    private static boolean hasGroupsSizeX(int[] deck) {
+        int[] nums = new int[10000];
+        for (int value : deck) {
+            nums[value]++;
+        }
+        int ans = -1;
+        for (int num : nums) {
+            if (num > 0) {
+                if (ans == -1) {
+                    ans = num;
+                } else {
+                    ans = gcd(ans, num);
+                }
+            }
+        }
+        return ans >= 2;
+    }
+
+    /**
+     * 公约数
+     *
+     * @param x 旧值
+     * @param y 新值
+     * @return 公约数
+     */
+    private static int gcd(int x, int y) {
+        return x == 0 ? y : gcd(y % x, x);
     }
 }
