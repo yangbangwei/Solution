4646e14db32ec1b3854468646311598e86e7d01a
yangbangwei <754667850@qq.com>
2020-01-10 10:24:56 +0800

    1037. 有效的回旋镖 核心点： 1.判断斜率是否在同一直线上。 2.(y2 - y1) * (x3 - x1) 与 (y3 - y1) * (x2 - x1)

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_10.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_10.java
@@ -36,4 +36,16 @@ public class Test_2020_01_10 {
         return total;
     }
 
+    /**
+     * 1037. 有效的回旋镖
+     * 回旋镖定义为一组三个点，这些点各不相同且不在一条直线上。
+     * 给出平面上三个点组成的列表，判断这些点是否可以构成回旋镖。
+     *
+     * @param points 坐标点
+     * @return 是否能构成回旋镖
+     */
+    private boolean isBoomerang(int[][] points) {
+        return (points[2][1] - points[0][1]) * (points[1][0] - points[0][0]) !=
+                (points[1][1] - points[0][1]) * (points[2][0] - points[0][0]);
+    }
 }
