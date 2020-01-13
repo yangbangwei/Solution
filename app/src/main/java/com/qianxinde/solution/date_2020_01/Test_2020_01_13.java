8567cdc1e09108c074bb2fd3b480088cf0060a51
yangbangwei <754667850@qq.com>
2020-01-13 14:17:49 +0800

    575. 分糖果 核心点： 1.比较糖果的种类数量，糖果的总数的二分之一。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_13.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_13.java
@@ -2,6 +2,7 @@ package com.qianxinde.solution.date_2020_01;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -200,5 +201,21 @@ public class Test_2020_01_13 {
             j--;
         }
         return true;
+    }
+
+    /**
+     * 575. 分糖果
+     * 给定一个偶数长度的数组，其中不同的数字代表着不同种类的糖果，每一个数字代表一个糖果。
+     * 你需要把这些糖果平均分给一个弟弟和一个妹妹。返回妹妹可以获得的最大糖果的种类数。
+     *
+     * @param candies 糖果种类
+     * @return 妹妹最大种类数
+     */
+    private int distributeCandies(int[] candies) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int candy : candies) {
+            hashSet.add(candy);
+        }
+        return Math.min(hashSet.size() / 2, candies.length / 2);
     }
 }
