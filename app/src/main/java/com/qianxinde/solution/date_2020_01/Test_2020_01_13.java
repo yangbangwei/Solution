cd6a5f0e3a9f22280e3ff54370479a80c2dee3be
yangbangwei <754667850@qq.com>
2020-01-13 15:23:34 +0800

    598. 范围求和 II 核心点： 1.判断每次操作，x，y的最小值。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_13.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_13.java
@@ -292,4 +292,24 @@ public class Test_2020_01_13 {
         }
         return max;
     }
+
+    /**
+     * 598. 范围求和 II
+     * 给定一个初始元素全部为 0，大小为 m*n 的矩阵 M 以及在 M 上的一系列更新操作。
+     * 操作用二维数组表示，其中的每个操作用一个含有两个正整数 a 和 b 的数组表示，
+     * 含义是将所有符合 0 <= i < a 以及 0 <= j < b 的元素 M[i][j] 的值都增加 1。
+     * 在执行给定的一系列操作后，你需要返回矩阵中含有最大整数的元素个数。
+     *
+     * @param m   矩阵m
+     * @param n   矩阵n
+     * @param ops 操作数组
+     * @return 最大整数的个数
+     */
+    private int maxCount(int m, int n, int[][] ops) {
+        for (int[] op : ops) {
+            m = Math.min(op[0], m);
+            n = Math.min(op[1], n);
+        }
+        return m * n;
+    }
 }
