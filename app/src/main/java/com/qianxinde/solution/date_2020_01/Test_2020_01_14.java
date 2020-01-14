459e01d368d42fa786c70e78bb7163b66a4b2a92
yangbangwei <754667850@qq.com>
2020-01-14 11:14:19 +0800

    876. 链表的中间结点 核心点： 1.数组存储每一个链表节点，取二分之一位置。 2.快慢指针。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_14.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_14.java
@@ -26,6 +26,7 @@ public class Test_2020_01_14 {
             return true;
         }
         List<Integer> list = new ArrayList<>();
+        list.add(root.val);
         dfs(root, list);
         int val = list.get(0);
         for (Integer integer : list) {
@@ -49,9 +50,54 @@ public class Test_2020_01_14 {
         if (root.left != null) {
             dfs(root.left, list);
         }
-        list.add(root.val);
         if (root.right != null) {
             dfs(root.right, list);
+        }
+    }
+
+    /**
+     * 876. 链表的中间结点
+     * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+     * 如果有两个中间结点，则返回第二个中间结点。
+     * 方法1，快慢指针
+     *
+     * @param head 链表
+     * @return 中间节点
+     */
+    private ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (slow != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 876. 链表的中间结点
+     * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+     * 如果有两个中间结点，则返回第二个中间结点。
+     * 方法2，保存到数组中
+     *
+     * @param head 链表
+     * @return 中间节点
+     */
+    private ListNode middleNode1(ListNode head) {
+        ListNode[] nodes = new ListNode[100];
+        int i = 0;
+        while (head != null) {
+            nodes[i++] = head;
+            head = head.next;
+        }
+        return nodes[i / 2];
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
         }
     }
 
