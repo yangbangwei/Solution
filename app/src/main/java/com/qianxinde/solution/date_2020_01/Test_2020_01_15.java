04deb73d5fb521d41adf030560d18e10e5709ec7
yangbangwei <754667850@qq.com>
2020-01-15 10:58:26 +0800

    896. 单调数列 核心点： 1.两个值，判断是否为单调递增或者递减。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_15.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_15.java
@@ -68,4 +68,28 @@ public class Test_2020_01_15 {
         }
         return list;
     }
+
+    /**
+     * 896. 单调数列
+     * 如果数组是单调递增或单调递减的，那么它是单调的。
+     * 如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。
+     * 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+     * 当给定的数组 A 是单调数组时返回 true，否则返回 false。
+     *
+     * @param A 数组
+     * @return 判断是否为单调数组
+     */
+    private boolean isMonotonic(int[] A) {
+        boolean isUp = true;
+        boolean isDown = true;
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i] > A[i + 1]) {
+                isUp = false;
+            }
+            if (A[i] < A[i + 1]) {
+                isDown = false;
+            }
+        }
+        return isUp || isDown;
+    }
 }
