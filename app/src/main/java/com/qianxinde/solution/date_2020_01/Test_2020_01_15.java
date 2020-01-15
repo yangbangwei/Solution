ca92dfd61972c234fa623143fe7b2201b3a4912e
yangbangwei <754667850@qq.com>
2020-01-15 15:01:26 +0800

    606. 根据二叉树创建字符串 核心点： 1.左子树不能省略。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_15.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_15.java
@@ -177,4 +177,35 @@ public class Test_2020_01_15 {
         }
         return num;
     }
+
+    /**
+     * 606. 根据二叉树创建字符串
+     * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+     * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+     *
+     * @param t 二叉树
+     * @return 转换的字符串
+     */
+    private String tree2str(TreeNode t) {
+        if (t == null) {
+            return "";
+        }
+        if (t.left == null && t.right == null) {
+            return t.val + "";
+        }
+        if (t.right == null) {
+            return t.val + "(" + tree2str(t.left) + ")";
+        }
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
 }
