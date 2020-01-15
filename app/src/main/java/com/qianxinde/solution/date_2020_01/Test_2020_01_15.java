28b53a63ccc0505f3d0d0de8c66e7d2b9f8e4244
yangbangwei <754667850@qq.com>
2020-01-15 13:54:51 +0800

    893. 特殊等价字符串组 核心点： 1.用set保存奇偶位置的和。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_15.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_15.java
@@ -15,6 +15,8 @@ public class Test_2020_01_15 {
         System.out.println(powerfulIntegers(2, 3, 10));
 
         System.out.println(readBinaryWatch(1));
+
+        System.out.println(numSpecialEquivGroups(new String[]{"abcd", "cdab", "adcb", "cbad"}));
     }
 
     /**
@@ -91,5 +93,35 @@ public class Test_2020_01_15 {
             }
         }
         return isUp || isDown;
+    }
+
+    /**
+     * 893. 特殊等价字符串组
+     * 你将得到一个字符串数组 A。
+     * 如果经过任意次数的移动，S == T，那么两个字符串 S 和 T 是特殊等价的。
+     * 一次移动包括选择两个索引 i 和 j，且 i ％ 2 == j ％ 2，交换 S[j] 和 S [i]。
+     * 现在规定，A 中的特殊等价字符串组是 A 的非空子集 S，
+     * 这样不在 S 中的任何字符串与 S 中的任何字符串都不是特殊等价的。
+     * 返回 A 中特殊等价字符串组的数量。
+     *
+     * @param A 字符串
+     * @return 等价字符串组的数量
+     */
+    private static int numSpecialEquivGroups(String[] A) {
+        HashSet<String> set = new HashSet<>();
+        for (String s : A) {
+            long a = 0;
+            long b = 0;
+            char[] words = s.toCharArray();
+            for (int i = 0; i < words.length; i++) {
+                if (i % 2 == 0) {
+                    a *= words[i];
+                } else {
+                    b *= words[i];
+                }
+            }
+            set.add(a + "+" + b);
+        }
+        return set.size();
     }
 }
