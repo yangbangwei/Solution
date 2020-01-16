89eec0926c59ce0174a4d4ed7d8ff3226085cc23
yangbangwei <754667850@qq.com>
2020-01-16 14:06:03 +0800

    985. 查询后的偶数和 核心点： 1.把偶数先累加，然后根据每次操作位置是否偶数。操作完是否为偶数。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_16.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_16.java
@@ -148,4 +148,40 @@ public class Test_2020_01_16 {
         }
         return ans;
     }
+
+    /**
+     * 985. 查询后的偶数和
+     * 给出一个整数数组 A 和一个查询数组 queries。
+     * 对于第 i 次查询，有 val = queries[i][0], index = queries[i][1]，我们会把 val 加到 A[index] 上。
+     * 然后，第 i 次查询的答案是 A 中偶数值的和。
+     * （此处给定的 index = queries[i][1] 是从 0 开始的索引，每次查询都会永久修改数组 A。）
+     * 返回所有查询的答案。你的答案应当以数组 answer 给出，answer[i] 为第 i 次查询的答案。
+     *
+     * @param A       整数数组
+     * @param queries 查询数组
+     * @return 查询结果
+     */
+    private int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int[] ans = new int[queries.length];
+        int total = 0;
+        for (int i : A) {
+            if (i % 2 == 0) {
+                total += i;
+            }
+        }
+        for (int i = 0; i < queries.length; i++) {
+            int index = queries[i][1];
+            int val = queries[i][0];
+            int old = A[index];
+            if (old % 2 == 0) {
+                total -= old;
+            }
+            A[index] += val;
+            if (A[index] % 2 == 0) {
+                total += A[index];
+            }
+            ans[i] = total;
+        }
+        return ans;
+    }
 }
