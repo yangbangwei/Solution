806389b512db56b05efb08788bfbae10903ddc6c
yangbangwei <754667850@qq.com>
2020-01-17 11:23:05 +0800

    235. 二叉搜索树的最近公共祖先 核心点： 1.递归调用，判断root的值同时大于/小于p，q。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_17.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_17.java
@@ -141,4 +141,25 @@ public class Test_2020_01_17 {
         }
         return diff.size();
     }
+
+    /**
+     * 235. 二叉搜索树的最近公共祖先
+     * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+     * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，
+     * 最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+     *
+     * @param root 二叉树
+     * @param p    左节点
+     * @param q    右节点
+     * @return 最近公共祖先
+     */
+    private TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
+    }
 }
