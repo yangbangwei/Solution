a06df891adf7bdec1c8d9efc7adc3d776b8f4f6a
yangbangwei <754667850@qq.com>
2020-01-17 11:37:35 +0800

    538. 把二叉搜索树转换为累加树 1.后序遍历，从right累加到left。

--- app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_17.java
+++ app/src/main/java/com/qianxinde/solution/date_2020_01/Test_2020_01_17.java
@@ -162,4 +162,25 @@ public class Test_2020_01_17 {
             return root;
         }
     }
+
+    private int add;
+
+    /**
+     * 538. 把二叉搜索树转换为累加树
+     * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，
+     * 使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+     *
+     * @param root 二叉树
+     * @return 累加树
+     */
+    private TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        convertBST(root.right);
+        root.val += add;
+        add = root.val;
+        convertBST(root.left);
+        return root;
+    }
 }
